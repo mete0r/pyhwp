3dd2bb5fe8e822fc438b89e43b4e706a7b40fcae
mete0r sarangbang <mete0r@sarangbang.or.kr>
2010-08-03 23:58:21 +0900

    gui: convert strings to unicode

--- src/wxhwp50html.py
+++ src/wxhwp50html.py
@@ -1,20 +1,28 @@
 # -*- coding: utf-8 -*-
 import wx
 
-app = wx.App()
-dlg = wx.FileDialog(None, '변환할 한글 5.0 파일을 고르십시오:', '', '*.hwp', 'HWP 파일(*.hwp)|*.hwp')
-ret = dlg.ShowModal()
+if __name__=='__main__':
+    app = wx.App()
+    dlg = wx.FileDialog(None, u'변환할 한글 5.0 파일을 고르십시오:', '', '*.hwp', u'HWP 파일(*.hwp)|*.hwp')
+    ret = dlg.ShowModal()
 
-if ret == 5100:
-    import hwp50, hwp50html
-    import os.path
-    doc = None
-    try:
-        hwpfilename = dlg.GetPath()
-        doc = hwp50.Document(hwpfilename)
-    except Exception, e:
-        print '변환할 수 없는 파일입니다..'+str(e)
-    if doc is not None:
-        rootname = os.path.splitext(os.path.basename(hwpfilename))[0]
-        cvt = hwp50html.HtmlConverter()
-        cvt.convert(doc, hwp50html.LocalDestination(rootname))
+    if ret == 5100:
+        import os.path
+        doc = None
+        try:
+            import hwp50, hwp50html
+            hwpfilename = dlg.GetPath()
+            doc = hwp50.Document(hwpfilename)
+        except Exception, e:
+            msgdlg = wx.MessageDialog(None, u'변환할 수 없는 파일입니다.'+unicode(e))
+            msgdlg.ShowModal()
+        if doc is not None:
+            try:
+                rootname = os.path.splitext(os.path.basename(hwpfilename))[0]
+                cvt = hwp50html.HtmlConverter()
+                cvt.convert(doc, hwp50html.LocalDestination(rootname))
+                msgdlg = wx.MessageDialog(None, u'변환하였습니다.')
+                msgdlg.ShowModal()
+            except Exception, e:
+                msgdlg = wx.MessageDialog(None, u'변환에 실패하였습니다:'+unicode(e))
+                msgdlg.ShowModal()
