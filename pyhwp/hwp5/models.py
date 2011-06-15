4415b166afe73cf0c9b1ea615e74fa7238aae090
mete0r sarangbang <mete0r@sarangbang.or.kr>
2011-06-16 00:42:42 +0900

    make table rows with cells in tablebody

--- pyhwp/hwp5/models.py
+++ pyhwp/hwp5/models.py
@@ -1664,7 +1664,7 @@ def pass3_field_start_end_pair(event_prefixed_cmas):
         else:
             yield event, cmas
 
-def pass3_listheader_paragraphs(event_prefixed_cmas):
+def pass3_listheader_paragraphs(event_prefixed_cmas, parentmodel=ListHeader, childmodel=Paragraph):
     ''' make paragraphs children of the listheader '''
     stack = []
     level = 0
@@ -1672,12 +1672,12 @@ def pass3_listheader_paragraphs(event_prefixed_cmas):
         (context, model, attributes, stream) = cmas
         if event is STARTEVENT:
             level += 1
-        if len(stack) > 0 and ((event is STARTEVENT and stack[-1][0] == level and model is not Paragraph) or
+        if len(stack) > 0 and ((event is STARTEVENT and stack[-1][0] == level and model is not childmodel) or
                                (event is ENDEVENT and stack[-1][0]-1 == level)):
             lh_level, lh_cmas = stack.pop()
             yield ENDEVENT, lh_cmas
 
-        if issubclass(model, ListHeader):
+        if issubclass(model, parentmodel):
             if event is STARTEVENT:
                 stack.append((level, cmas))
                 yield event, cmas
@@ -1689,11 +1689,50 @@ def pass3_listheader_paragraphs(event_prefixed_cmas):
         if event is ENDEVENT:
             level -= 1
 
+def pass3_tablebody(event_prefixed_cmas):
+    from collections import deque
+    class TableRow: pass
+    stack = []
+    for event, item in event_prefixed_cmas:
+        (context, model, attributes, stream) = item
+        if model is TableBody:
+            if event is STARTEVENT:
+                rowcols = deque()
+                for cols in attributes.pop('rowcols'):
+                    if cols == 1:
+                        rowcols.append(3)
+                    else:
+                        rowcols.append(1)
+                        for i in range(0, cols-2):
+                            rowcols.append(0)
+                        rowcols.append(2)
+                stack.append((context, rowcols))
+                yield event, item
+            else:
+                yield event, item
+                stack.pop()
+        elif model is TableCell:
+            table_context, rowcols = stack[-1]
+            row_context = dict(table_context)
+            if event is STARTEVENT:
+                how = rowcols[0]
+                if how & 1:
+                    yield STARTEVENT, (row_context, TableRow, dict(), None)
+            yield event, item
+            if event is ENDEVENT:
+                how = rowcols.popleft()
+                if how & 2:
+                    yield ENDEVENT, (row_context, TableRow, dict(), None)
+        else:
+            yield event, item
+
 def parse_models_pass3(event_prefixed_cmas):
     event_prefixed_cmas = pass3_lineseg_charshaped_texts(event_prefixed_cmas)
     event_prefixed_cmas = pass3_inline_extended_controls(event_prefixed_cmas)
     event_prefixed_cmas = pass3_field_start_end_pair(event_prefixed_cmas)
     event_prefixed_cmas = pass3_listheader_paragraphs(event_prefixed_cmas)
+    event_prefixed_cmas = pass3_listheader_paragraphs(event_prefixed_cmas, TableBody, TableCell)
+    event_prefixed_cmas = pass3_tablebody(event_prefixed_cmas)
     return event_prefixed_cmas
 
 def parse_models(context, records, passes=3):
