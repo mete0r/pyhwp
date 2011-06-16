9dc05e06cc99f520d73f831dde1a356c10418fc2
mete0r sarangbang <mete0r@sarangbang.or.kr>
2011-06-16 22:57:39 +0900

    reorganize hwpxml.py

--- pyhwp/hwp5/hwpxml.py
+++ pyhwp/hwp5/hwpxml.py
@@ -98,6 +98,76 @@ def element(context, xmlgen, (model, attributes)):
     for x in startelement(context, xmlgen, (model, attributes)): yield x
     yield xmlgen.endElement, model.__name__
 
+from xml.sax.saxutils import XMLGenerator
+from .models import ModelEventHandler, Text
+from .dataio import hexdump
+class XmlFormat(ModelEventHandler):
+    def __init__(self, out):
+        self.xmlgen = XMLGenerator(out, 'utf-8')
+    def startDocument(self):
+        self.xmlgen.startDocument()
+    def startModel(self, model, attributes, **context):
+        context['logging'].debug('hwpxml.XmlFormat: model: %s, %s', model.__name__, attributes)
+        context['logging'].debug('hwpxml.XmlFormat: context: %s', context)
+        recordid = context.get('recordid', ('UNKNOWN', 'UNKNOWN', -1))
+        hwptag = context.get('hwptag', '')
+        context['logging'].info('hwpxml.XmlFormat: rec:%d %s', recordid[2], hwptag)
+        if model is Text:
+            text = attributes.pop('text')
+        else:
+            text = None
+
+        for x in startelement(context, self.xmlgen, (model, attributes)): x[0](*x[1:])
+
+        if model is Text and text is not None:
+            self.xmlgen.characters(text)
+
+        unparsed = context.get('unparsed', '')
+        if len(unparsed) > 0:
+            context['logging'].debug('UNPARSED: %s', hexdump(unparsed, True))
+    def endModel(self, model):
+        self.xmlgen.endElement(model.__name__)
+    def endDocument(self):
+        self.xmlgen.endDocument()
+
+
+def flatxml(hwpfile, logger, oformat):
+    ''' convert hwpfile into a flat xml
+
+    hwpfile - hwp file
+    oformat - output formatter
+    '''
+    from .recordstream import read_records
+    from .models import parse_models, wrap_modelevents
+    from .models import create_context
+    from .models import dispatch_model_events
+    context = create_context(hwpfile, logging=logger)
+
+    class HwpDoc(object): pass
+    class DocInfo(object): pass
+    class BodyText(object): pass
+    hwpdoc = HwpDoc, dict(version=hwpfile.fileheader.version), dict(context)
+    docinfo = DocInfo, dict(), dict(context)
+    docinfo_records = read_records(hwpfile.docinfo(), 'docinfo')
+    docinfo_events = wrap_modelevents(docinfo, parse_models(context, docinfo_records))
+
+    bodytext = BodyText, dict(), dict(context)
+    bodytext_events = []
+    for idx in hwpfile.list_bodytext_sections():
+        section_records = read_records(hwpfile.bodytext(idx), 'bodytext/%d'%idx)
+        section_events = parse_models(context, section_records)
+        bodytext_events.append(section_events)
+    bodytext_events = chain(*bodytext_events)
+    bodytext_events = wrap_modelevents(bodytext, bodytext_events)
+
+    hwpdoc_events = chain(docinfo_events, bodytext_events)
+    hwpdoc_events = wrap_modelevents(hwpdoc, hwpdoc_events)
+
+    oformat.startDocument()
+    dispatch_model_events(oformat, hwpdoc_events)
+    oformat.endDocument()
+
+
 def main():
     import sys
     import logging
@@ -110,89 +180,28 @@ def main():
 
     options, args = op.parse_args()
 
+    filename = args_pop(args, 'filename')
+    hwpfile = File(filename)
+
     out = options.outfile
 
-    filename = args_pop(args, 'filename')
-    file = File(filename)
-
-    from xml.sax.saxutils import XMLGenerator
-    xmlgen = XMLGenerator(out, 'utf-8')
-    from .models import ModelEventHandler, Text
-    from .dataio import hexdump
-    class XmlFormat(ModelEventHandler):
-        def startDocument(self):
-            xmlgen.startDocument()
-        def startModel(self, model, attributes, **context):
-            if options.loglevel <= logging.DEBUG:
-                xmlgen._write('<!-- hwpxml.XmlFormat: model: %s, %s -->'%(model.__name__, attributes))
-                xmlgen._write('<!-- hwpxml.XmlFormat: context: %s -->'%context)
-            recordid = context.get('recordid', ('UNKNOWN', 'UNKNOWN', -1))
-            hwptag = context.get('hwptag', '')
-            if options.loglevel <= logging.INFO:
-                xmlgen._write('<!-- hwpxml.XmlFormat: rec:%d %s -->'%(recordid[2], hwptag))
-            if model is Text:
-                text = attributes.pop('text')
-            else:
-                text = None
-
-            for x in startelement(context, xmlgen, (model, attributes)): x[0](*x[1:])
-
-            if model is Text and text is not None:
-                xmlgen.characters(text)
-            if options.loglevel <= logging.INFO:
-                unparsed = context.get('unparsed', '')
-                if len(unparsed) > 0:
-                    xmlgen._write('<!-- UNPARSED\n')
-                    xmlgen._write(hexdump(unparsed, True))
-                    xmlgen._write('\n-->')
-        def endModel(self, model):
-            xmlgen.endElement(model.__name__)
-        def endDocument(self):
-            xmlgen.endDocument()
-
     class NulFormat(ModelEventHandler):
+        def __init__(self, out): pass
         def startDocument(self): pass
         def endDocument(self): pass
         def startModel(self, model, attributes, **context): pass
         def endModel(self, model): pass
 
     formats = dict(xml=XmlFormat, nul=NulFormat)
-    oformat = formats[options.format]()
+    oformat = formats[options.format](out)
 
-    from .recordstream import read_records
-    from .models import parse_models, wrap_modelevents
-    from .models import create_context
-    from .models import dispatch_model_events
     from ._scriptutils import getlogger, loghandler, logformat_xml
     if options.format == 'xml':
         logger = getlogger(options, loghandler(out, logformat_xml))
     else:
         logger = getlogger(options)
-    context = create_context(file, logging=logger)
-
-    class HwpDoc(object): pass
-    class DocInfo(object): pass
-    class BodyText(object): pass
-    hwpdoc = HwpDoc, dict(filename=filename, version=file.fileheader.version), dict(context)
-    docinfo = DocInfo, dict(), dict(context)
-    docinfo_records = read_records(file.docinfo(), 'docinfo', filename)
-    docinfo_events = wrap_modelevents(docinfo, parse_models(context, docinfo_records))
-
-    bodytext = BodyText, dict(), dict(context)
-    bodytext_events = []
-    for idx in file.list_bodytext_sections():
-        section_records = read_records(file.bodytext(idx), 'bodytext/%d'%idx, filename)
-        section_events = parse_models(context, section_records)
-        bodytext_events.append(section_events)
-    bodytext_events = chain(*bodytext_events)
-    bodytext_events = wrap_modelevents(bodytext, bodytext_events)
-
-    hwpdoc_events = chain(docinfo_events, bodytext_events)
-    hwpdoc_events = wrap_modelevents(hwpdoc, hwpdoc_events)
-
-    oformat.startDocument()
-    dispatch_model_events(oformat, hwpdoc_events)
-    oformat.endDocument()
+    flatxml(hwpfile, logger, oformat)
+    
 
 if __name__ == '__main__':
     main()
