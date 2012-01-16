c93a59819cac401615b95c7a2875c882b15691c0
mete0r <mete0r@sarangbang.or.kr>
2012-01-17 06:39:38 +0900

    oxt: 최초로 작동하는 hwp5 필터 구현

--- oxt/pyhwp.py
+++ oxt/pyhwp.py
@@ -36,6 +36,16 @@ class Fac(object):
     def __init__(self, context):
         self.context = context
 
+    @property
+    def storage_factory(self):
+        return self.context.ServiceManager.createInstance('com.sun.star.embed.StorageFactory')
+
+    def StorageFromInputStream(self, inputstream):
+        return self.storage_factory.createInstanceWithArguments( (inputstream, 1) ) # com.sun.star.embed.ElementModes.READ
+
+    def StorageFromStream(self, stream):
+        return self.storage_factory.createInstanceWithArguments( (stream, 4) ) # com.sun.star.embed.ElementModes.READ
+
     def OLESimpleStorage(self, *args):
         return self.context.ServiceManager.createInstanceWithArguments('com.sun.star.embed.OLESimpleStorage', args)
 
@@ -43,11 +53,166 @@ class Fac(object):
         return self.context.ServiceManager.createInstanceWithArguments('com.sun.star.io.SequenceInputStream',
                 (uno.ByteSequence(s),) )
 
+    def UriReferenceFactory(self):
+        return self.context.ServiceManager.createInstanceWithContext('com.sun.star.uri.UriReferenceFactory', self.context)
+
+    def rdf_URI_create(self, uri):
+        return self.context.ServiceManager.createInstanceWithArguments('com.sun.star.rdf.URI', (uri, ))
+
+    def createBaseURI(self, storage, baseuri, subdoc):
+        return self.rdf_URI_create(baseuri+'/')
+
+    def readThroughComponent(self, storage, doc, streamname, filtername, filterargs, name):
+        stream = storage.openStreamElement(streamname, 1) # READ
+        return self.readStreamThroughComponent(stream, doc, filtername, filterargs, name)
+
+    def readStreamThroughComponent(self, stream, doc, filtername, filterargs, name):
+        from com.sun.star.xml.sax import InputSource
+        inputsource = InputSource()
+        inputsource.sSystemId = name
+        inputsource.aInputStream = stream
+        inputsource.sEncoding = 'utf-8'
+
+        parser = self.saxParser()
+        filter = self.context.ServiceManager.createInstanceWithArguments(filtername, filterargs)
+        filter.setTargetDocument(doc)
+        parser.setDocumentHandler(filter)
+        parser.parseStream(inputsource)
+        return True
+
+    def PropertySet(self):
+        return self.context.ServiceManager.createInstance('com.sun.star.beans.PropertySet')
+
+    def load_odt_from_storage(self, doc, storage, statusindicator=None):
+        infoset = self.PropertySet()
+        url = ''
+        uri = self.createBaseURI(storage, url, '')
+        doc.loadMetadataFromStorage(storage, uri, None)
+        #self.readThroughComponent(storage, doc, 'meta.xml', 'com.sun.star.comp.Writer.XMLOasisMetaImporter', (infoset, None), '')
+        graphicresolver = None
+        objectresolver = None
+        lateinitsettings = None
+        filterargs = (infoset, statusindicator, graphicresolver, objectresolver, lateinitsettings)
+        self.readThroughComponent(storage, doc, 'styles.xml', 'com.sun.star.comp.Writer.XMLOasisStylesImporter', filterargs, '')
+        self.readThroughComponent(storage, doc, 'content.xml', 'com.sun.star.comp.Writer.XMLOasisContentImporter', filterargs, '')
+
     def saxParser(self):
         return self.context.ServiceManager.createInstance('com.sun.star.xml.sax.Parser')
 
     def WriterXMLImporter(self):
         return self.context.ServiceManager.createInstance('com.sun.star.comp.Writer.XMLImporter')
+
+    def WriterXMLContentImporter(self):
+        return self.context.ServiceManager.createInstance('com.sun.star.comp.Writer.XMLContentImporter')
+
+    def TempFile(self):
+        return self.context.ServiceManager.createInstance('com.sun.star.io.TempFile')
+
+    def HwpFileFromInputStream(self, inputstream):
+        olestorage = self.OLESimpleStorage(inputstream)
+        olefile = OleFileIO_from_OLESimpleStorage(olestorage)
+        from hwp5.filestructure import File
+        return File(olefile)
+
+    def TempStorage(self):
+        tempfile = self.TempFile()
+        return self.StorageFromStream(self.TempFile())
+
+    def hwp5file_convert_to_odtpkg_file(self, hwp5file):
+        from tempfile import TemporaryFile
+        tmpfile = TemporaryFile()
+        import os
+        tmpfile2 = os.fdopen( os.dup(tmpfile.fileno()), 'r')
+
+        from zipfile import ZipFile
+        zf = ZipFile(tmpfile, 'w')
+        from hwp5.hwp5odt import ODTPackage
+        odtpkg = ODTPackage(zf)
+        try:
+            from hwp5.hwp5odt import hwp5file_convert_to_odtpkg
+            hwp5file_convert_to_odtpkg(hwp5file, odtpkg)
+        finally:
+            odtpkg.close()
+
+        tmpfile2.seek(0)
+        return tmpfile2
+
+    def PropertySequence(self, **kwargs):
+        return dict_to_propseq(kwargs)
+
+    def flatxml(self, hwp5file):
+        import tempfile
+        from hwp5.hwp5odt import generate_hwp5xml
+        from hwp5.hwp5odt import xsltproc, xsl
+
+        hwp5xml_file = tempfile.TemporaryFile()
+        generate_hwp5xml(hwp5xml_file, hwp5file)
+        return hwp5xml_file
+
+    def hwpfile_to_doc(self, hwpfile, doc):
+        import tempfile
+        from hwp5.hwp5odt import generate_hwp5xml
+        from hwp5.hwp5odt import xsltproc, xsl
+
+        hwpxml = tempfile.TemporaryFile()
+        try:
+            generate_hwp5xml(hwpxml, hwpfile)
+
+            try:
+                hwpxml.seek(0)
+                content = tempfile.TemporaryFile()
+                xsltproc(xsl.content)(hwpxml, content)
+
+                content.seek(0)
+                inputstream = InputStreamFromFileLike(content)
+                self.parse_odt_content_stream_into_doc(inputstream, doc)
+            finally:
+                content.close()
+        finally:
+            hwpxml.close()
+
+    def xslted_stream(self, xsltproc, xsl_filename, infile):
+        transform = xsltproc(xsl_filename)
+
+        import os
+        i, o = os.pipe()
+        pipein = os.fdopen(i, 'r')
+        pipeout = os.fdopen(o, 'w')
+        def run():
+            logging.debug('XSLT starts...')
+            transform(infile, pipeout)
+            logging.debug('XSLT end')
+            pipeout.close()
+            logging.debug('XSLT pipeout closed')
+
+        def detached_run(run):
+            import threading
+            t = threading.Thread(target=run)
+            t.daemon = True
+            logging.debug('XSLT thread starting...')
+            t.start()
+
+        detached_run(log_exception(run))
+        return InputStreamFromFileLike(pipein)
+
+    def hwp5xml_odt_content_stream(self, hwp5xml_file):
+        from hwp5.hwp5odt import xsltproc, xsl
+        return self.xslted_stream(xsltproc, xsl.content, hwp5xml_file)
+
+    def parse_stream_into_doc(self, stream, dochandler, doc):
+        from com.sun.star.xml.sax import InputSource
+        inputsource = InputSource()
+        inputsource.aInputStream = stream
+        inputsource.sEncoding = 'utf-8'
+
+        dochandler.setTargetDocument(doc)
+
+        parser = self.saxParser()
+        parser.setDocumentHandler(dochandler)
+        parser.parseStream(inputsource)
+
+    def parse_odt_content_stream_into_doc(self, stream, doc):
+        return self.parse_stream_into_doc(stream, self.WriterXMLContentImporter(), doc)
 
     def parse_fodt_stream_into_doc(self, stream, doc):
         from com.sun.star.xml.sax import InputSource
@@ -61,6 +226,22 @@ class Fac(object):
         parser = self.saxParser()
         parser.setDocumentHandler(dochandler)
         parser.parseStream(inputsource)
+
+    def SAXParser(self, dochandler):
+        parser = self.saxParser()
+        parser.setDocumentHandler(dochandler)
+        def consume(stream):
+            from com.sun.star.xml.sax import InputSource
+            inputsource = InputSource()
+            inputsource.aInputStream = stream
+            inputsource.sEncoding = 'utf-8'
+            parser.parseStream(inputsource)
+        return consume
+
+    def FlatXMLImporter(self, doc):
+        handler = self.WriterXMLImporter()
+        handler.setTargetDocument(doc)
+        return self.SAXParser(handler)
 
 class File_Stream(object):
     def __init__(self, stream):
@@ -94,9 +275,46 @@ class File_Stream(object):
                 stream.seek(offset)
             self.seek = seek
 
+        if hasattr(stream, 'writeBytes'):
+            def write(s):
+                stream.writeBytes(uno.ByteSequence(s))
+            self.write = write
+
+            def flush():
+                stream.flush()
+            self.flush = flush
+
     def close(self):
-        if hasattr(stream, 'closeInput'):
+        if hasattr(self.stream, 'closeInput'):
             self.stream.closeInput()
+        if hasattr(self.stream, 'closeOutput'):
+            self.stream.closeOutput()
+
+def container_recurse_elements(parent, parent_path_segments):
+    for name in parent.getElementNames():
+        elem = parent.getByName(name)
+
+        path_segments = parent_path_segments + [name]
+        yield path_segments
+
+        if hasattr(elem, 'getElementNames'):
+            for x in container_recurse_elements(elem, path_segments):
+                yield x
+
+def container_find_element(parent, path_segments):
+    if len(path_segments) == 0:
+        return parent
+
+    if not hasattr(parent, 'getByName'):
+        return None
+
+    from com.sun.star.container import NoSuchElementException
+    child_name = path_segments[0]
+    try:
+        child = parent.getByName(child_name)
+    except NoSuchElementException:
+        return None
+    return container_find_element(child, path_segments[1:])
 
 class OleFileIO_from_OLESimpleStorage(object):
     def __init__(self, storage):
@@ -105,13 +323,15 @@ class OleFileIO_from_OLESimpleStorage(object):
     def exists(self, name):
         return self.storage.hasByName(name)
 
-    def openstream(self, name):
+    def listdir(self):
+        for x in container_recurse_elements(self.storage, []):
+            yield x
+
+    def openstream(self, path):
         from com.sun.star.container import NoSuchElementException
-        try:
-            stream = self.storage.getByName(name)
-        except NoSuchElementException:
-            return None
-        else:
+        path_segments = path.split('/')
+        stream = container_find_element(self.storage, path_segments)
+        if stream is not None:
             return File_Stream(stream)
 
 def unofy_value(value):
@@ -128,6 +348,69 @@ def dict_to_propseq(d):
 
 def propseq_to_dict(propvalues):
     return dict((p.Name, p.Value) for p in propvalues)
+
+from com.sun.star.io import XInputStream, XSeekable
+class InputStreamFromFileLike(unohelper.Base, XInputStream, XSeekable):
+    def __init__(self, f):
+        self.f = f
+
+    def readBytes(self, aData, nBytesToRead):
+        logging.debug('InputStream.readBytes(%d)', nBytesToRead)
+        data = self.f.read(nBytesToRead)
+        return len(data), uno.ByteSequence(data)
+
+    readSomeBytes = readBytes
+
+    def skipBytes(self, nBytesToSkip):
+        logging.debug('InputStream.skipBytes(%d)', nBytesToSkip)
+        data = self.f.read(nBytesToSkip)
+
+    def available(self):
+        logging.debug('InputStream.available()')
+        return 0
+
+    def closeInput(self):
+        logging.debug('InputStream.close()')
+        self.f.close()
+
+    def seek(self, location):
+        logging.debug('InputStream.seek(%d)', location)
+        self.f.seek(location)
+
+    def getPosition(self):
+        pos = self.f.tell()
+        logging.debug('InputStream.getPosition(): %d', pos)
+        return pos
+
+    def getLength(self):
+        pos = self.f.tell()
+        try:
+            self.f.seek(0, 2)
+            length = self.f.tell()
+            logging.debug('InputStream.getLength(): %d', length)
+            return length
+        finally:
+            self.f.seek(pos)
+
+class ODTPackage(object):
+    def __init__(self, storage):
+        self.storage = storage
+
+    def insert_stream(self, f, path, media_type):
+        print path, media_type
+        storage = self.storage
+        WRITE = 4 # = com.sun.star.embed.ElementModes.WRITE
+
+        path_segments = path.split('/')
+        intermediates = path_segments[:-1]
+        name = path_segments[-1]
+        for segment in intermediates:
+            storage = storage.openStorageElement(segment, WRITE)
+        stream = storage.openStreamElement(name, WRITE)
+        File_Stream(stream).write(f.read())
+
+    def close(self):
+        self.storage.commit()
 
 @implementation('pyhwp.Detector', 'com.sun.star.document.ExtendedTypeDetection')
 class Detector(unohelper.Base, XExtendedFilterDetection):
@@ -195,22 +478,21 @@ class Importer(unohelper.Base, XInitialization, XFilter, XImporter):
         statusindicator = desc.get('StatusIndicator')
 
         inputstream = desc['InputStream']
-        olestorage = self.fac.OLESimpleStorage(inputstream)
-        olefile = OleFileIO_from_OLESimpleStorage(olestorage)
-        from hwp5.filestructure import File
-        hwpfile = File(olefile)
+        hwpfile = self.fac.HwpFileFromInputStream(inputstream)
 
-        xmldata = '''<?xml version="1.0" encoding="utf-8"?>
-<office:document xmlns:office="http://openoffice.org/2000/office"
-    xmlns:text="http://openoffice.org/2000/text"
-    office:class="text">
-    <office:body>
-        <text:p>Hello world!</text:p>
-    </office:body>
-</office:document>
-'''
-        fodt_stream = self.fac.SequenceInputStream(xmldata)
-        self.fac.parse_fodt_stream_into_doc(fodt_stream, self.target)
+        odtpkg_file = self.fac.hwp5file_convert_to_odtpkg_file(hwpfile)
+        logging.debug('hwp to odtpkg completed')
+
+        #odtpkg_stream = InputStreamFromFileLike(odtpkg_file)
+
+        temp = self.fac.TempFile()
+        File_Stream(temp).write(odtpkg_file.read())
+        temp.seek(0)
+        odtpkg_stream = temp
+
+        odtpkg_storage = self.fac.StorageFromInputStream(odtpkg_stream)
+
+        self.fac.load_odt_from_storage(self.target, odtpkg_storage, statusindicator)
         return True
 
     def cancel(self):
