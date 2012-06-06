122f65e25d985feeceeda734d45ba73718ee277a
mete0r <mete0r@sarangbang.or.kr>
2012-06-06 15:28:16 +0900

    hwp5proc: docopt (0.3) 사용하여 재구성

--- pyhwp/hwp5/proc.py
+++ pyhwp/hwp5/proc.py
@@ -1,186 +1,95 @@
 # -*- coding: utf-8 -*-
-import sys
-from . import filestructure
-from OleFileIO_PL import OleFileIO
-from .filestructure import OleStorage
+'''HWP format version 5 processor.
 
-class Context(object):
-    def __init__(self, sys):
-        self.sys = sys
+Usage:
+    hwp5proc ls [--with-extra] <hwp5file>
+    hwp5proc cat [--with-extra] <hwp5file> <stream>
+    hwp5proc unpack [--with-extra] <hwp5file> [<out-directory>]
+    hwp5proc version <hwp5file>
+    hwp5proc -h | --help
+    hwp5proc --version
 
-    @property
-    def prog(self):
-        return self.sys.argv[0]
+Options:
+    -h --help       Show this screen
+    --version       Show version
+    --with-extra    process with extra parsed items
+'''
 
-    @property
-    def progname(self):
-        import os.path
-        return os.path.split(self.prog)[-1]
-
-    @property
-    def args_options(self):
-        options = dict()
-        args = list()
-        for arg in self.sys.argv[1:]:
-            if arg.startswith('--'):
-                option = arg[2:].split('=', 1)
-                if len(option) == 1:
-                    k = option[0]
-                    v = '1'
-                else:
-                    k, v = option
-                options[k] = v
-            else:
-                args.append(arg)
-        return args, options
-
-    @property
-    def args(self):
-        return self.args_options[0]
-
-    @property
-    def options(self):
-        return self.args_options[1]
-
-    def shift(self):
-        return self.sys.argv.pop(0)
+import logging
 
 
-class ProcContext(Context):
+logger = logging.getLogger(__name__)
 
-    @property
-    def filename(self):
-        if len(self.args) == 0:
-            raise 'Filename is required'
-        return self.args[0]
-
-    @property
-    def olefile(self):
-        return OleFileIO(self.filename)
-
-    @property
-    def olestorage(self):
-        return OleStorage(self.olefile)
-
-    @property
-    def hwp5file_fs(self):
-        return filestructure.Hwp5File(self.olestorage)
-
-    @property
-    def hwp5file_rec(self):
-        from . import recordstream
-        return recordstream.Hwp5File(self.olestorage)
-
-    @property
-    def hwp5file_bin(self):
-        from . import binmodel
-        return binmodel.Hwp5File(self.olestorage)
-
-    @property
-    def hwp5file_xml(self):
-        from . import xmlmodel
-        return xmlmodel.Hwp5File(self.olestorage)
-
-    hwp5file = hwp5file_xml
-
-    @property
-    def storage(self):
-        layer = self.options.get('layer')
-        if layer == 'ole':
-            storage = self.olestorage
-        elif layer == 'fs':
-            storage = self.hwp5file_fs
-        elif layer == 'rec':
-            storage = self.hwp5file_rec
-        elif layer == 'bin':
-            storage = self.hwp5file_bin
-        elif layer == 'xml':
-            storage = self.hwp5file_xml
-        else:
-            storage = self.hwp5file
-
-        if not self.options.get('no-extra'):
-            from .storage import ExtraItemStorage
-            storage = ExtraItemStorage(storage)
-        return storage
-
-    @property
-    def stream(self):
-        from .storage import open_storage_item
-        return open_storage_item(self.storage, self.args[1])
-
-    @property
-    def outdir_or_operand_root(self):
-        outdir = self.options.get('outdir', None)
-        if outdir:
-            return outdir
-
-        import os.path
-        outdir, ext = os.path.splitext(os.path.basename(self.filename))
-        return outdir
-
-
-context = ProcContext(sys)
 
 def main():
-    if context.progname == 'hwp5proc':
-        context.shift()
-
-    if context.progname == 'version':
-        program = version
-    elif context.progname == 'unpack':
-        program = unpack
-    elif context.progname == 'ls':
-        program = ls
-    elif context.progname == 'cat':
-        program = cat
-    else:
-        print 'unknown command: ', context.progname
-        help()
-        return
-
-    program()
+    from docopt import docopt
+    from pkg_resources import get_distribution
+    dist = get_distribution('pyhwp')
+    args = docopt(__doc__, version=dist.version)
+    if args['version']:
+        version(args)
+    elif args['ls']:
+        ls(args)
+    elif args['cat']:
+        cat(args)
+    elif args['unpack']:
+        unpack(args)
 
 
-def help():
-    print 'help'
+def version(args):
+    from .xmlmodel import Hwp5File
+    hwp5file = Hwp5File(args['<hwp5file>'])
+    h = hwp5file.fileheader
+    #print h.signature.replace('\x00', ''),
+    print '%d.%d.%d.%d' % h.version
 
 
-def version():
-    filename = context.args[0]
-
-    olefile = OleFileIO(filename)
-    olestg = OleStorage(olefile)
-    hwp5file = filestructure.Hwp5File(olestg)
-
-    h = hwp5file.header
-    print h.signature.replace('\x00', ''), '%d.%d.%d.%d'%h.version
-
-
-def unpack():
-    outdir = context.outdir_or_operand_root
-
-    import os, os.path
-    if not os.path.exists(outdir):
-        os.mkdir(outdir)
-    filestructure.unpack(context.storage, outdir)
-
-
-def ls():
+def ls(args):
+    from .xmlmodel import Hwp5File
+    from .storage import ExtraItemStorage
     from .storage import printstorage
-    printstorage(context.storage)
+    hwpfile = Hwp5File(args['<hwp5file>'])
+    if args['--with-extra']:
+        hwpfile = ExtraItemStorage(hwpfile)
+    printstorage(hwpfile)
 
 
-def cat():
-    f = context.stream
+def cat(args):
+    from .xmlmodel import Hwp5File
+    from .storage import ExtraItemStorage
+    from .storage import open_storage_item
+    import sys
+    hwp5file = Hwp5File(args['<hwp5file>'])
+    if args['--with-extra']:
+        hwp5file = ExtraItemStorage(hwp5file)
+    stream = open_storage_item(hwp5file, args['<stream>'])
+    f = stream.open()
     try:
-        if hasattr(f, 'read'):
-            while True:
-                data = f.read(4096)
-                if data:
-                    sys.stdout.write(data)
-                else:
-                    return
+        while True:
+            data = f.read(4096)
+            if data:
+                sys.stdout.write(data)
+            else:
+                return
     finally:
         if hasattr(f, 'close'):
             f.close()
+
+
+def unpack(args):
+    from .xmlmodel import Hwp5File
+    from .storage import ExtraItemStorage
+    from . import filestructure
+    import os, os.path
+
+    filename = args['<hwp5file>']
+    hwp5file = Hwp5File(filename)
+    if args['--with-extra']:
+        hwp5file = ExtraItemStorage(hwp5file)
+
+    outdir = args['<out-directory>']
+    if outdir is None:
+        outdir, ext = os.path.splitext(os.path.basename(filename))
+    if not os.path.exists(outdir):
+        os.mkdir(outdir)
+    filestructure.unpack(hwp5file, outdir)
--- pyhwp/setup.py
+++ pyhwp/setup.py
@@ -26,7 +26,7 @@ setup(
         packages = ['hwp5'],
         package_data = dict(hwp5=['xsl/*.xsl', 'odf-relaxng/OpenDocument-v1.2-os-*.rng']),
 
-        install_requires=['OleFileIO_PL >=0.20', 'simplejson'],
+        install_requires=['OleFileIO_PL >=0.20', 'simplejson', 'docopt == 0.3'],
         extras_require=dict(test=['lxml']),
 
         entry_points = {
