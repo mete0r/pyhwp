f33879214138befcc828d6fdbe93fd052c60aa84
mete0r <mete0r@sarangbang.or.kr>
2012-06-10 14:50:48 +0900

    refactor test-oxt

--- misc/test-oxt.py
+++ misc/test-oxt.py
@@ -1,48 +1,64 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-def with_soffice_headless(f):
-    uno_url = 'pipe,name=pyhwp;urp;'
-    args = ['soffice', '--nologo', '--headless', '--invisible','--norestore',
-            '--nodefault', '--nolockcheck', '--nofirstwizard',
-            '--accept=%s' % uno_url]
+from contextlib import contextmanager
+
+def get_remote_context(uno_link, max_tries=3):
+    import uno
+    import time
+    while True:
+        max_tries -= 1
+        try:
+            localcontext = uno.getComponentContext()
+            localsm = localcontext.ServiceManager
+            resolver_name = 'com.sun.star.bridge.UnoUrlResolver'
+            resolver = localsm.createInstanceWithContext(resolver_name, localcontext)
+
+            return resolver.resolve('uno:'+uno_link+'StarOffice.ComponentContext')
+        except Exception, e:
+            name = type(e).__name__
+            noconnect = 'com.sun.star.connection.NoConnectException'
+            if name == noconnect:
+                if max_tries <= 0:
+                    raise
+                print name, '- retrying'
+                time.sleep(2)
+                continue
+            raise
+
+
+@contextmanager
+def soffice(**kwargs):
+    args = ['soffice', '--nologo', '--norestore', '--nodefault',
+            '--nofirstwizard']
+
+    if 'pipe' in kwargs:
+        pipe = kwargs['pipe']
+    else:
+        pipe = 'pyhwp'
+    uno_link = 'pipe,name={0};urp;'.format(pipe)
+    args.append('--accept={0}'.format(uno_link))
+    
+    if kwargs.get('headless', True):
+        args.extend(['--headless', '--invisible'])
+
     import subprocess
     p = subprocess.Popen(args)
-    try:
-        # waiting for soffice to be ready (guessed)
-        import time
-        trycount = 0
-        while trycount < 3:
-            trycount += 1
-            try:
-                f(uno_url)
-                break
-            except Exception, e:
-                name = type(e).__name__
-                print name
-                noconnect = 'com.sun.star.connection.NoConnectException'
-                if name == noconnect:
-                    time.sleep(2)
-                    continue
-                raise
-    finally:
-        p.terminate()
+    yield get_remote_context(uno_link)
+    p.terminate()
 
-def runtest(uno_url):
-    import uno
-    localcontext = uno.getComponentContext()
-    localsm = localcontext.ServiceManager
-    resolver_name = 'com.sun.star.bridge.UnoUrlResolver'
-    resolver = localsm.createInstanceWithContext(resolver_name, localcontext)
 
-    context = resolver.resolve('uno:'+uno_url+'StarOffice.ComponentContext')
+def runtest(context):
     sm = context.ServiceManager
 
     testjob = sm.createInstanceWithContext('pyhwp.TestJob', context)
     testjob.trigger('${buildout:parts-directory}/test')
 
+
 def main():
-    with_soffice_headless(runtest)
+    with soffice() as context:
+        runtest(context)
+
 
 if __name__ == '__main__':
     main()
