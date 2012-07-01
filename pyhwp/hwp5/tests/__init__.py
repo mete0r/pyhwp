9605aa4c8cc3c68aba7127d9e6aa477eab1c5c46
mete0r <mete0r@sarangbang.or.kr>
2012-07-01 23:48:30 +0900

    tests ordering: highlevel first, lowlevel last

--- pyhwp/hwp5/tests/__init__.py
+++ pyhwp/hwp5/tests/__init__.py
@@ -1,21 +1,17 @@
 def test_suite():
     from unittest import defaultTestLoader as TL
     from unittest import TestSuite as TS
-    import test_treeop
-    import test_binmodel
-    import test_dataio
-    import test_xmlmodel
-    import test_xmlformat
-    import test_filestructure
-    import test_storage
-    import test_recordstream
-    import test_odtxsl
-    tests = [test_storage, test_filestructure, test_binmodel, test_dataio, test_xmlmodel, test_xmlformat, test_odtxsl]
-    tests.append(test_recordstream)
-    tests.append(test_treeop)
-
-    import test_externprogs
-    tests[0:0] = [test_externprogs]
+    tests = []
+    tests.append(__import__('test_odtxsl'))
+    tests.append(__import__('test_externprogs'))
+    tests.append(__import__('test_xmlformat'))
+    tests.append(__import__('test_xmlmodel'))
+    tests.append(__import__('test_binmodel'))
+    tests.append(__import__('test_recordstream'))
+    tests.append(__import__('test_filestructure'))
+    tests.append(__import__('test_dataio'))
+    tests.append(__import__('test_treeop'))
+    tests.append(__import__('test_storage'))
 
     # localtest: a unittest module which resides at the local test site only;
     # should not be checked in the source code repository
