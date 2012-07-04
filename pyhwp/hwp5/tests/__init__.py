3392024a6eff37c85ef440bf34c5c6ec6ef7fb6f
mete0r <mete0r@sarangbang.or.kr>
2012-07-05 04:22:34 +0900

    don't use __import__ in tests: test-oxt fails
    I don't know why.

--- pyhwp/hwp5/tests/__init__.py
+++ pyhwp/hwp5/tests/__init__.py
@@ -2,16 +2,26 @@ def test_suite():
     from unittest import defaultTestLoader as TL
     from unittest import TestSuite as TS
     tests = []
-    tests.append(__import__('test_odtxsl'))
-    tests.append(__import__('test_externprogs'))
-    tests.append(__import__('test_xmlformat'))
-    tests.append(__import__('test_xmlmodel'))
-    tests.append(__import__('test_binmodel'))
-    tests.append(__import__('test_recordstream'))
-    tests.append(__import__('test_filestructure'))
-    tests.append(__import__('test_dataio'))
-    tests.append(__import__('test_treeop'))
-    tests.append(__import__('test_storage'))
+    from hwp5.tests import test_odtxsl as T
+    tests.append(T)
+    from hwp5.tests import test_externprogs as T
+    tests.append(T)
+    from hwp5.tests import test_xmlformat as T
+    tests.append(T)
+    from hwp5.tests import test_xmlmodel as T
+    tests.append(T)
+    from hwp5.tests import test_binmodel as T
+    tests.append(T)
+    from hwp5.tests import test_recordstream as T
+    tests.append(T)
+    from hwp5.tests import test_filestructure as T
+    tests.append(T)
+    from hwp5.tests import test_dataio as T
+    tests.append(T)
+    from hwp5.tests import test_treeop as T
+    tests.append(T)
+    from hwp5.tests import test_storage as T
+    tests.append(T)
 
     # localtest: a unittest module which resides at the local test site only;
     # should not be checked in the source code repository
