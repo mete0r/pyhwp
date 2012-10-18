7a6f4f9eed25821c1855613d71cb21bb7c60e762
mete0r <mete0r@sarangbang.or.kr>
2012-10-19 04:15:25 +0900

    unokit: singletons

---
+++ unokit/unokit/tests/test_singletons.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+#
+#                   GNU AFFERO GENERAL PUBLIC LICENSE
+#                      Version 3, 19 November 2007
+#
+#   pyhwp : hwp file format parser in python
+#   Copyright (C) 2010 mete0r@sarangbang.or.kr
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+from unittest import TestCase
+from unokit.remote import RemoteContextLayer
+
+
+class TestBase(TestCase):
+
+    layer = RemoteContextLayer
+
+
+class TestSingletons(TestBase):
+    def test_extman(self):
+        from unokit.singletons import css
+        extman = css.deployment.ExtensionManager
+        self.assertTrue(extman is not None)
+
+    def test_pkginfo_prov(self):
+        from unokit.singletons import css
+        pkginfo_prov = css.deployment.PackageInformationProvider
+        self.assertTrue(pkginfo_prov is not None)
+
+        exts = list(pkginfo_prov.ExtensionList)
+        extids = list(extid for extid, extver in exts)
+        extid = 'com.sun.star.script.provider.ScriptProviderForPython'
+        self.assertTrue(extid in extids)
+        ext_loc = pkginfo_prov.getPackageLocation(extid)
+        self.assertTrue(ext_loc != '')
