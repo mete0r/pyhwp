2d3d08562826f2a757d1f38737df138921a05992
mete0r <mete0r@sarangbang.or.kr>
2012-11-16 18:55:29 +0900

    plat/xmllint: provides relaxng with external xmllint

---
+++ pyhwp/hwp5/plat/xmllint.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+#
+#   pyhwp : hwp file format parser in python
+#   Copyright (C) 2010 mete0r@sarangbang.or.kr
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+from __future__ import with_statement
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+executable = 'xmllint'
+enabled = False
+
+
+def is_enabled():
+    return enabled
+
+
+def enable():
+    global enabled
+    enabled = True
+
+
+def disable():
+    global enabled
+    enabled = False
+
+
+def relaxng(rng_path, inp_path):
+   from subprocess import Popen
+   args = [executable, '--noout', '--relaxng', rng_path, inp_path]
+   p = Popen(args)
+   p.wait()
+   return p.returncode == 0
