f6c83e7ee99eecdd177d1881bb6cad4b7c8a4c6b
mete0r <mete0r@sarangbang.or.kr>
2012-11-19 02:03:16 +0900

    unokit: setup working dir

--- unokit/setup.py
+++ unokit/setup.py
@@ -1,3 +1,10 @@
-from setuptools import setup, find_packages
-setup(name='unokit',
-      packages=find_packages())
+import os
+import os.path
+cwd = os.getcwd()
+os.chdir(os.path.dirname(__file__))
+try:
+    from setuptools import setup, find_packages
+    setup(name='unokit',
+          packages=find_packages())
+finally:
+    os.chdir(cwd)
