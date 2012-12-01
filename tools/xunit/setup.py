5d190920d8f68cfcba599d635b40f5b9573ad198
mete0r <mete0r@sarangbang.or.kr>
2012-12-02 03:17:20 +0900

    xunit: remove explicit dependency on lxml to ease buildout
    Currently xunit requires lxml, but we omit it to make buildout process
    successful even without it. This workaround should be resolved with
    Issue #101.

--- tools/xunit/setup.py
+++ tools/xunit/setup.py
@@ -5,7 +5,10 @@ setup_dir = os.path.dirname(__file__)
 os.chdir(setup_dir)
 setup(name='xunit',
       packages=find_packages(),
-      install_requires=['pyhwp', 'lxml', 'docopt'],
+      # TODO: lxml is required, but we omit it to make buildout process
+      # successful without it. This workaround should be resolved with Issue
+      # #101.
+      install_requires=['pyhwp', 'docopt'],
       entry_points={
           'console_scripts': [
               'xunit = xunit:main'
