94024f794b207935a81cce9be631b82ecc6ef024
mete0r <mete0r@sarangbang.or.kr>
2012-12-02 09:10:12 +0900

    pyhwp-tests: split hwp5.tests out from pyhwp
    tox 테스트 시, 각 testenv에 설치된 hwp5를 사용하도록 하기 위해 테스트를
    별도 디렉토리로 분리한다.
    
    nose로 테스트 시 테스트 파일들이 있는 디렉토리를 지정하는데,
    hwp5.tests가 hwp5와 같은 디렉토리 즉 {프로젝트-루트}/pyhwp에 있으면
    테스트 실행 시 함께 있는 hwp5 패키지가 사용될 수 밖에 없다.

--- pyhwp-tests/hwp5_tests/fixtures/__init__.py
+++ pyhwp-tests/hwp5_tests/fixtures/__init__.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-
 
 def get_fixture_path(filename):
-    import os.path
-    path = os.path.join('fixtures', filename)
     from hwp5.importhelper import pkg_resources_filename
-    return pkg_resources_filename('hwp5.tests', path)
+    return pkg_resources_filename(__name__, filename)
 
 
 def open_fixture(filename, *args, **kwargs):
