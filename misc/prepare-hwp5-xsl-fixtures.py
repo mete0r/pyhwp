5669154ef13b4bead56fd35b4f8f80dc0f80b0d2
mete0r <mete0r@sarangbang.or.kr>
2012-12-04 13:37:31 +0900

    tox: replace pyhwp-xsltests package with misc/prepare-hwp5-xsl-fixtures

--- misc/prepare-hwp5-xsl-fixtures.py
+++ misc/prepare-hwp5-xsl-fixtures.py
@@ -12,7 +12,7 @@ def find_hwp5files(dir):
     return glob.glob(os.path.join(dir, '*.hwp'))
 
 
-def prepare():
+def main():
     doc = ''' convert fixture hwp5 files into *.xml
 
     Usage:
@@ -46,6 +46,8 @@ def prepare():
         rootname = os.path.splitext(name)[0]
         out_path = rootname + '.xml'
         if out_dir is not None:
+            if not os.path.exists(out_dir):
+                os.makedirs(out_dir)
             out_path = os.path.join(out_dir, out_path)
 
         logger.info('%s', out_path)
@@ -57,3 +59,8 @@ def prepare():
                 hwp5file.xmlevents(**opts).dump(f)
         except Exception, e:
             logger.exception(e)
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main())
