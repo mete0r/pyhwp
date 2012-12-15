e48a449ea798bb4d2570065b1ee2d7edbcdb8a11
mete0r <mete0r@sarangbang.or.kr>
2012-12-15 17:10:14 +0900

    oxt.tool: remove obsolete oxt-make

--- tools/oxt.tool/setup.py
+++ tools/oxt.tool/setup.py
@@ -9,7 +9,6 @@ try:
           packages=find_packages(),
           entry_points = {
               'console_scripts': [
-                  'oxt-make = oxt_tool:make',
                   'run-in-lo = oxt_tool:run_in_lo',
                   'oxt-storage-ls = oxt_tool.storage:ls_main',
                   'oxt-storage-put = oxt_tool.storage:put_main',
