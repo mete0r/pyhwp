d31b74a377a834d7a5787e9bac8f0474f56fd57b
mete0r <mete0r@sarangbang.or.kr>
2014-01-18 06:53:16 +0900

    buildout: generate buildout.cfg

---
+++ bootstrap_me.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+import os.path
+import sys
+
+
+def main():
+    role = len(sys.argv) > 1 and sys.argv[1] or 'maintainer'
+
+    this_dir = os.path.dirname(__file__)
+    dst_path = os.path.join(this_dir, 'buildout.cfg')
+    with file(dst_path, 'w') as f:
+        f.write('[buildout]\n')
+        f.write('extends = buildouts/' + role + '.cfg\n')
+
+
+if __name__ == '__main__':
+    main()
