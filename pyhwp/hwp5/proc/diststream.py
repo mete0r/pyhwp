c4918f5926a1145c1baf5b9f0145e16223ec4329
mete0r <mete0r@sarangbang.or.kr>
2014-01-31 18:16:51 +0900

    hwp5proc: diststream command

--- pyhwp/hwp5/proc/diststream.py
+++ pyhwp/hwp5/proc/diststream.py
@@ -20,7 +20,9 @@
 
 Usage::
 
+    hwp5proc diststream
     hwp5proc diststream sha1 [--raw]
+    hwp5proc diststream key [[--raw]
     hwp5proc diststream [--loglevel=<loglevel>] [--logfile=<logfile>]
     hwp5proc diststream --help
 
@@ -29,12 +31,26 @@ Options::
     -h --help               Show this screen
        --loglevel=<level>   Set log level.
        --logfile=<file>     Set log file.
+
+Example::
+
+    $ hwp5proc cat --ole samples/viewtext.hwp ViewText/Section0
+      | tee Section0.zraw.aes128ecb | hwp5proc diststream | tee Section0.zraw
+      | hwp5proc rawunz > Section0
+
+    $ hwp5proc diststream sha1 < Section0.zraw.aes128ecb
+    $ echo -n '12345' | sha1sum
+
 '''
 from binascii import b2a_hex
+from binascii import a2b_hex
 import logging
 
 from hwp5.proc import entrypoint
+from hwp5.distdoc import decode
 from hwp5.distdoc import decode_head_to_sha1
+from hwp5.distdoc import decode_head_to_key
+from hwp5.recordstream import read_record
 
 
 logger = logging.getLogger(__name__)
@@ -43,16 +59,21 @@ logger = logging.getLogger(__name__)
 @entrypoint(__doc__)
 def main(args):
     import sys
-
-    # skip record header
-    sys.stdin.read(4)
-
-    payload = sys.stdin.read(256)
+    import shutil
 
     if args['sha1']:
-        result = decode_head_to_sha1(payload)
-
-    if not args['--raw']:
-        result = b2a_hex(result)
-
-    sys.stdout.write(result)
+        head = read_record(sys.stdin, 0)
+        sha1ucs16le = decode_head_to_sha1(head['payload'])
+        sha1 = a2b_hex(sha1ucs16le.decode('utf-16le'))
+        if not args['--raw']:
+            sha1 = b2a_hex(sha1)
+        sys.stdout.write(sha1)
+    elif args['key']:
+        head = read_record(sys.stdin, 0)
+        key = decode_head_to_key(head['payload'])
+        if not args['--raw']:
+            key = b2a_hex(key)
+        sys.stdout.write(key)
+    else:
+        result = decode(sys.stdin)
+        shutil.copyfileobj(result, sys.stdout)
