ded8d8e77b6522775ce1003b757bf350959868f7
mete0r <mete0r@sarangbang.or.kr>
2014-02-25 01:14:21 +0900

    binmodel: enhance HWPTAG_NUMBERING

--- pyhwp/hwp5/binmodel/tagid23_numbering.py
+++ pyhwp/hwp5/binmodel/tagid23_numbering.py
@@ -21,12 +21,18 @@ from hwp5.tagids import HWPTAG_NUMBERING
 from hwp5.dataio import Enum
 from hwp5.dataio import Flags
 from hwp5.dataio import UINT32
+from hwp5.dataio import UINT16
+from hwp5.dataio import INT32
+from hwp5.dataio import BSTR
 from hwp5.dataio import HWPUNIT16
+from hwp5.dataio import Struct
+from hwp5.dataio import ARRAY
 
 
-class Numbering(RecordModel):
-    tagid = HWPTAG_NUMBERING
-    Align = Enum(LEFT=0, CENTER=1, RIGHT=2)
+class NumberingLevel(Struct):
+
+    # 표 35 문단 머리 정보 속성
+    Align = Enum(LEFT=0, CENTER=1, RIGHT=2, UNKNOWN=3)
     DistanceType = Enum(RATIO=0, VALUE=1)
     Flags = Flags(UINT32,
                   0, 1, Align, 'paragraph_align',
@@ -34,9 +40,24 @@ class Numbering(RecordModel):
                   3, 'auto_dedent',
                   4, DistanceType, 'distance_to_body_type')
 
+    @classmethod
     def attributes(cls):
         yield cls.Flags, 'flags'
         yield HWPUNIT16, 'width_correction'
         yield HWPUNIT16, 'distance_to_body'
-        yield UINT32, 'charshape_id'  # SPEC ?????
+        yield INT32, 'charshape_id'
+        yield BSTR, 'numbering_format'
+
+
+class Numbering(RecordModel):
+    ''' 4.1.8. 문단 번호 '''
+    tagid = HWPTAG_NUMBERING
+
+    Align = NumberingLevel.Align
+    DistanceType = NumberingLevel.DistanceType
+    Flags = NumberingLevel.Flags
+
+    def attributes(cls):
+        yield ARRAY(NumberingLevel, 7), 'levels'
+        yield UINT16, 'starting_number'
     attributes = classmethod(attributes)
