02a11646d3a9e861e50a92f5f7a72a6e3961250b
mete0r <mete0r@sarangbang.or.kr>
2014-11-09 13:02:40 +0900

    hwp5proc: enhance xml output on terminal
    - optional c14n with xmllint
    - optional format with xmllint
    - optional syntax highlight with pygment
    - optional pager with less

--- pyhwp/hwp5/proc/xml.py
+++ pyhwp/hwp5/proc/xml.py
@@ -61,8 +61,22 @@ Example::
 
 '''
 from __future__ import with_statement
+from contextlib import contextmanager
+from functools import partial
+import logging
+import subprocess
+
 from hwp5.proc import entrypoint
 from hwp5.xmldump_flat import xmldump_flat
+
+
+logger = logging.getLogger(__name__)
+
+
+def xmldump_nested(hwp5file, output, embedbin=False, xml_declaration=True):
+    dump = hwp5file.xmlevents(embedbin=embedbin).dump
+    dump = partial(dump, xml_declaration=xml_declaration)
+    dump(output)
 
 
 @entrypoint(__doc__)
@@ -72,25 +86,143 @@ def main(args):
     import sys
     from hwp5.xmlmodel import Hwp5File
 
-    opts = dict()
-    opts['embedbin'] = args['--embedbin']
+    fmt = args['--format'] or 'nested'
+    if fmt == 'flat':
+        xmldump = partial(xmldump_flat, xml_declaration=args['--no-xml-decl'])
+    elif fmt == 'nested':
+        xmldump = partial(xmldump_nested, embedbin=args['--embedbin'],
+                          xml_declaration=args['--no-xml-decl'])
 
     if args['--output']:
         output = open(args['--output'], 'w')
     else:
         output = sys.stdout
 
-    if args['--no-xml-decl']:
-        xml_declaration = False
-    else:
-        xml_declaration = True
-
-    fmt = args['--format'] or 'nested'
-
+    hwp5file = Hwp5File(args['<hwp5file>'])
     with output:
-        hwp5file = Hwp5File(args['<hwp5file>'])
-        if fmt == 'flat':
-            xmldump_flat(hwp5file, output, xml_declaration=xml_declaration)
-        elif fmt == 'nested':
-            hwp5file.xmlevents(**opts).dump(output,
-                                            xml_declaration=xml_declaration)
+
+        isatty = output.isatty()
+
+        output_wrappers = [
+            xmllint(encode='utf-8', format=isatty),
+            xmllint(c14n=True),
+        ]
+
+        if isatty:
+            output_wrappers = [
+                pager(),
+                syntaxhighlight('application/xml'),
+            ] + output_wrappers
+
+        with cascade_contextmanager_filters(output, output_wrappers) as output:
+            xmldump(hwp5file, output)
+
+
+@contextmanager
+def cascade_contextmanager_filters(arg, filters):
+    if len(filters) == 0:
+        yield arg
+    else:
+        flt, filters = filters[0], filters[1:]
+        with flt(arg) as ret:
+            with cascade_contextmanager_filters(ret, filters) as ret:
+                yield ret
+
+
+@contextmanager
+def null_contextmanager_filter(output):
+    yield output
+
+
+def output_thru_subprocess(cmd):
+    @contextmanager
+    def filter(output):
+        logger.debug('%r', cmd)
+        try:
+            p = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=output)
+        except Exception, e:
+            logger.error('%r: %s', ' '.join(cmd), e)
+            yield output
+        else:
+            try:
+                yield p.stdin
+            except IOError, e:
+                import errno
+                if e.errno != errno.EPIPE:
+                    raise
+            finally:
+                p.stdin.close()
+                p.wait()
+                retcode = p.returncode
+                logger.debug('%r exit %d', cmd, retcode)
+    return filter
+
+
+def xmllint(c14n=False, encode=None, format=False):
+    cmd = ['xmllint']
+    if c14n:
+        cmd.append('--c14n')
+    if encode:
+        cmd += ['--encode', encode]
+    if format:
+        cmd.append('--format')
+    cmd.append('-')
+    return output_thru_subprocess(cmd)
+
+
+def syntaxhighlight(mimetype):
+    try:
+        return syntaxhighlight_pygments(mimetype)
+    except Exception, e:
+        logger.info(e)
+        return null_contextmanager_filter
+
+
+def syntaxhighlight_pygments(mimetype):
+    from pygments import highlight
+    from pygments.lexers import get_lexer_for_mimetype
+    from pygments.formatters import TerminalFormatter
+
+    lexer = get_lexer_for_mimetype(mimetype, encoding='utf-8')
+    formatter = TerminalFormatter(encoding='utf-8')
+
+    @contextmanager
+    def filter(output):
+        with make_temp_file() as f:
+            yield f
+            f.seek(0)
+            code = f.read()
+        highlight(code, lexer, formatter, output)
+    return filter
+
+
+@contextmanager
+def make_temp_file():
+    import tempfile
+    import os
+    fd, name = tempfile.mkstemp()
+    with unlink_path(name):
+        with os.fdopen(fd, 'w+') as f:
+            yield f
+
+
+@contextmanager
+def unlink_path(path):
+    import os
+    try:
+        yield
+    finally:
+        os.unlink(path)
+
+
+def pager():
+    import os
+    import shlex
+    pager_cmd = os.environ.get('PAGER')
+    if pager_cmd:
+        pager_cmd = shlex.split(pager_cmd)
+        return output_thru_subprocess(pager_cmd)
+    return pager_less
+
+
+pager_less = output_thru_subprocess(['less', '-R'])
