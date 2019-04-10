effafa6f5c7cf08af238d59b465be453f2baa8f4
mete0r <mete0r@sarangbang.or.kr>
2019-04-10 23:10:43 +0900

    Fix lint error

--- pyhwp/hwp5/hwp5proc.py
+++ pyhwp/hwp5/hwp5proc.py
@@ -20,4 +20,4 @@ from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import unicode_literals
 
-from .proc import main_argparser
+from .proc import main_argparser  # noqa -- for building docs
